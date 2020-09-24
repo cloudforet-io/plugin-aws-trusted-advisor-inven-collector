48c81f890385eae7373e244f73847bb53778c7ea
Jihyung Song <bluese@megazone.com>
2020-09-24 16:12:14 +0900

    add html layouts in libs/schema

--- src/spaceone/inventory/connector/aws_trusted_advisor_connector/schema/resource.py
+++ src/spaceone/inventory/connector/aws_trusted_advisor_connector/schema/resource.py
@@ -3,7 +3,7 @@ from schematics.types import ModelType, StringType, PolyModelType, DictType, Lis
 from spaceone.inventory.connector.aws_trusted_advisor_connector.schema.data import Check
 from spaceone.inventory.libs.schema.resource import CloudServiceResource, CloudServiceResponse, CloudServiceMeta
 from spaceone.inventory.libs.schema.dynamic_field import TextDyField, ListDyField, BadgeDyField, EnumDyField
-from spaceone.inventory.libs.schema.dynamic_layout import ItemDynamicLayout, TableDynamicLayout, SimpleTableDynamicLayout
+from spaceone.inventory.libs.schema.dynamic_layout import ItemDynamicLayout, TableDynamicLayout, HTMLDynamicLayout
 
 resources = TableDynamicLayout.set_fields('Affected Resources', 'data.flagged_resources')
 resources.type = 'raw-table'
@@ -13,18 +13,23 @@ check = ItemDynamicLayout.set_fields('Check Information', fields=[
     TextDyField.data_source('Category', 'data.category'),
     TextDyField.data_source('Status', 'data.status'),
     TextDyField.data_source('Checked at', 'data.timestamp'),
-    TextDyField.data_source('Description', 'data.description')
+    # TextDyField.data_source('Description', 'data.description')
 ])
 
-metadata = CloudServiceMeta.set_layouts(layouts=[check, resources])
+html_description = HTMLDynamicLayout.set('Description', root_path='data.description')
+
+metadata = CloudServiceMeta.set_layouts(layouts=[check, html_description, resources])
+
 
 class SupportResource(CloudServiceResource):
     cloud_service_group = StringType(default='Support')
+
 
 class CheckResource(SupportResource):
     cloud_service_type = StringType(default='TrustedAdvisor')
     data = ModelType(Check)
     _metadata = ModelType(CloudServiceMeta, default=metadata, serialized_name='metadata')
+
 
 class CheckResponse(CloudServiceResponse):
     resource = PolyModelType(CheckResource)
