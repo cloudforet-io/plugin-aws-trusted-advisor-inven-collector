9135a1064e283ca5a0ffbae2ad07cfaf5bdf2cdf
Jihyung Song <bluese@megazone.com>
2020-12-21 13:59:24 +0900

    change status EnumField in dynamic metadata

--- src/spaceone/inventory/model/trusted_advisor/cloud_service.py
+++ src/spaceone/inventory/model/trusted_advisor/cloud_service.py
@@ -1,7 +1,7 @@
 from schematics.types import ModelType, StringType, PolyModelType
 
 from spaceone.inventory.model.trusted_advisor.data import Check
-from spaceone.inventory.libs.schema.metadata.dynamic_field import TextDyField
+from spaceone.inventory.libs.schema.metadata.dynamic_field import TextDyField, EnumDyField
 from spaceone.inventory.libs.schema.metadata.dynamic_layout import ItemDynamicLayout, TableDynamicLayout, \
     HTMLDynamicLayout
 from spaceone.inventory.libs.schema.cloud_service import CloudServiceResource, CloudServiceResponse, CloudServiceMeta
@@ -12,7 +12,12 @@ resources.type = 'raw-table'
 check = ItemDynamicLayout.set_fields('Check Information', fields=[
     TextDyField.data_source('Name', 'data.name'),
     TextDyField.data_source('Category', 'data.category'),
-    TextDyField.data_source('Status', 'data.status'),
+    EnumDyField.data_source('Status', 'data.status', default_state={
+        'safe': ['ok'],
+        'warning': ['warning'],
+        'alert': ['error'],
+        'disable': ['not_available']
+    }),
     TextDyField.data_source('Checked at', 'data.timestamp'),
 ])
 
