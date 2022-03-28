d62a497230097d04741b273b323ae31447af9d4d
Jihyung Song <bluese@megazone.com>
2022-03-28 15:01:23 +0900

    refactor: update exception logger message for information

--- src/spaceone/inventory/connector/trusted_advisor.py
+++ src/spaceone/inventory/connector/trusted_advisor.py
@@ -15,13 +15,11 @@ class TrustedAdvisorConnector(AWSConnector):
     def describe_trusted_advisor_checks(self, language, **query):
         query = self.generate_query(is_paginate=False, **query)
         response = self.client.describe_trusted_advisor_checks(language=language, **query)
-
         return response.get('checks', [])
 
     def describe_trusted_advisor_check_result(self, check_id, language, **query):
         query = self.generate_query(is_paginate=False, **query)
         response = self.client.describe_trusted_advisor_check_result(checkId=check_id, language=language, **query)
-
         return response.get('result', {})
 
     def refresh_trusted_advisor_check(self, check_id, **query):
@@ -31,4 +29,4 @@ class TrustedAdvisorConnector(AWSConnector):
             query = self.generate_query(is_paginate=False, **query)
             self.client.refresh_trusted_advisor_check(checkId=check_id, **query)
         except Exception as e:
-            pass
+            _LOGGER.info(f"[refresh_trusted_advisor_check] {e}")
