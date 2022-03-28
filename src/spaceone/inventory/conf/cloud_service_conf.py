a4958c6443ae82211e55d06c94dedfb1543ed7ca
Jihyung Song <bluese@megazone.com>
2022-03-28 15:00:30 +0900

    refactor: add cloud_service_conf.py

---
+++ src/spaceone/inventory/conf/cloud_service_conf.py
@@ -0,0 +1,11 @@
+MAX_WORKER = 20
+SUPPORTED_FEATURES = ['garbage_collection']
+SUPPORTED_RESOURCE_TYPE = ['inventory.CloudService', 'inventory.CloudServiceType']
+SUPPORTED_SCHEDULES = ['hours']
+DEFAULT_REGION = 'us-east-1'
+FILTER_FORMAT = []
+EXECUTE_MANAGER = 'TrustedAdvisorManager'
+ARN_DEFAULT_PARTITION = 'aws'
+DEFAULT_LANGUAGE = 'ko'
+DEFAULT_REFRESH = True
+
