0b569ecfc90c3cc65deffd4caf1320c51249c721
Jihyung Song <bluese@megazone.com>
2022-03-28 15:02:02 +0900

    feat: add error resource

--- src/spaceone/inventory/manager/trustedadvisor_manager.py
+++ src/spaceone/inventory/manager/trustedadvisor_manager.py
@@ -1,14 +1,17 @@
 import time
+import json
+import logging
+
 from spaceone.inventory.libs.manager import AWSManager
 from spaceone.inventory.libs.schema.base import ReferenceModel
+from spaceone.inventory.libs.schema.error_resource import ErrorResourceResponse
 from spaceone.inventory.connector.trusted_advisor import TrustedAdvisorConnector
 from spaceone.inventory.model.trusted_advisor.data import Check, CheckId
 from spaceone.inventory.model.trusted_advisor.cloud_service import CheckResource, CheckResponse
 from spaceone.inventory.model.trusted_advisor.cloud_service_type import CLOUD_SERVICE_TYPES
+from spaceone.inventory.conf.cloud_service_conf import *
 
-DEFAULT_REGION = 'us-east-1'
-DEFAULT_LANGUAGE = 'ko'
-DEFAULT_REFRESH = True
+_LOGGER = logging.getLogger(__name__)
 
 
 class TrustedAdvisorManager(AWSManager):
@@ -16,7 +19,7 @@ class TrustedAdvisorManager(AWSManager):
     cloud_service_types = CLOUD_SERVICE_TYPES
 
     def collect_cloud_services(self, params):
-        print("** Trusted Advisor Start **")
+        _LOGGER.debug("** Trusted Advisor Start **")
         start_time = time.time()
         ta_conn: TrustedAdvisorConnector = self.locator.get_connector(self.connector_name, **params)
         ta_conn.set_client()
@@ -25,55 +28,61 @@ class TrustedAdvisorManager(AWSManager):
         need_refresh = params.get('options', {}).get('refresh', DEFAULT_REFRESH)
 
         ta_resources = []
+        event_resources = []
 
         for check in ta_conn.describe_trusted_advisor_checks(language):
-            check_id_data = CheckId(check, strict=False)
             check_id = check['id']
+            arn = self.generate_arn(service='support', region=DEFAULT_REGION,
+                                    account_id=params['account_id'], resource_type="trusted_advisor",
+                                    resource_id=check_id)
 
-            if need_refresh:
-                ta_conn.refresh_trusted_advisor_check(check_id)
+            try:
+                check_id_data = CheckId(check, strict=False)
 
-            check_result = ta_conn.describe_trusted_advisor_check_result(check_id, language)
+                if need_refresh:
+                    ta_conn.refresh_trusted_advisor_check(check_id)
 
-            if not check_result:
-                # Nothing to do
-                continue
+                check_result = ta_conn.describe_trusted_advisor_check_result(check_id, language)
 
-            # if status != "ok", there exists flagged resources
-            # Processing flagged resource for easy viewing(a.k.a. TableDynamic)
-            # if raw['status'] != "ok":
-            #    print(checkId.metadata)
-            #    print(raw)
-            #    import time
-            #    time.sleep(5)
-            # raw.update({'headers': ['a','b','c'], 'flagged_resources': [[1,2,3], [4,5,6]]})
-            # Change 1.1
-            # { 'flagged_resources': [{'a': 1, 'b': 2, 'c': 3}, {'a': 4, 'b': 5, 'c': 6}] }
+                if not check_result:
+                    # Nothing to do
+                    continue
 
-            flagged_resources = self._merge_flagged_resources(check_id_data, check_result)
-            check_result.update({'flagged_resources': flagged_resources})
-            check_result.update({
-                'name': check_id_data.name,
-                'category': check_id_data.category,
-                'description': check_id_data.description,
-                'arn': self.generate_arn(service='support', region=DEFAULT_REGION,
-                                         account_id=params['account_id'], resource_type="trusted_advisor",
-                                         resource_id=check_id),
-                'account_id': params['account_id']
-            })
+                # if status != "ok", there exists flagged resources
+                # Processing flagged resource for easy viewing(a.k.a. TableDynamic)
+                # if raw['status'] != "ok":
+                #    print(checkId.metadata)
+                #    print(raw)
+                #    import time
+                #    time.sleep(5)
+                # raw.update({'headers': ['a','b','c'], 'flagged_resources': [[1,2,3], [4,5,6]]})
+                # Change 1.1
+                # { 'flagged_resources': [{'a': 1, 'b': 2, 'c': 3}, {'a': 4, 'b': 5, 'c': 6}] }
 
-            check_data = Check(check_result, strict=False)
+                flagged_resources = self._merge_flagged_resources(check_id_data, check_result)
+                check_result.update({'flagged_resources': flagged_resources})
+                check_result.update({
+                    'name': check_id_data.name,
+                    'category': check_id_data.category,
+                    'description': check_id_data.description,
+                    'arn': arn,
+                    'account_id': params['account_id']
+                })
 
-            check_resource = CheckResource({
-                'name': check_data.name,
-                'data': check_data,
-                'region_code': 'global',
-                'reference': ReferenceModel(check_data.reference())
-            })
+                check_data = Check(check_result, strict=False)
 
-            ta_resources.append(CheckResponse({'resource': check_resource}))
+                check_resource = CheckResource({
+                    'name': check_data.name,
+                    'data': check_data,
+                    'region_code': 'global',
+                    'reference': ReferenceModel(check_data.reference())
+                })
 
-        print(f' Trusted Advisor Finished {time.time() - start_time} Seconds')
+                ta_resources.append(CheckResponse({'resource': check_resource}))
+            except Exception as e:
+                event_resources.append(self.generate_error(arn, e))
+
+        _LOGGER.debug(f' Trusted Advisor Finished {time.time() - start_time} Seconds')
         return ta_resources
 
     @staticmethod
@@ -105,3 +114,22 @@ class TrustedAdvisorManager(AWSManager):
                 data[headers[idx]] = res[idx]
             resources.append(data)
         return resources
+
+    def generate_error(self, resource_arn, error_message):
+        _LOGGER.error(f'[generate_error] {error_message}')
+
+        if isinstance(error_message, dict):
+            error_resource_response = ErrorResourceResponse(
+                {'message': json.dumps(error_message),
+                 'resource': {'resource_id': resource_arn,
+                              'cloud_service_group': self.cloud_service_group,
+                              'cloud_service_type': self.cloud_service_type}})
+
+        else:
+            error_resource_response = ErrorResourceResponse(
+                {'message': str(error_message),
+                 'resource': {'resource_id': resource_arn,
+                              'cloud_service_group': self.cloud_service_group,
+                              'cloud_service_type': self.cloud_service_type}})
+
+        return error_resource_response
